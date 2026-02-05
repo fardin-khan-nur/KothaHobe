0fe5e78a711ceeb1e45b1e507a9fbb7c32a6a66c
Fardin Khan Nur <133893300+fardin-khan-nur@users.noreply.github.com>
2026-02-06 04:05:47 +0600

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,17 +1,17 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import path from "path"
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
-      "@shared": path.resolve(__dirname, "../shared"),
-    },
+      "@shared": path.resolve(__dirname, "../shared")
+    }
   },
   build: {
     outDir: "dist",
-    emptyOutDir: true,
-  },
-});
+    emptyOutDir: true
+  }
+})
